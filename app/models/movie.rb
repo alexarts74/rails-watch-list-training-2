61a0bd4d2e1a883987ea5fd8523c903218346f2e
alexandre artus <artusalexandre@yahoo.fr>
2023-03-28 12:44:42 +0200

    watchlist quasi fini

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   has_many :lists, through: :bookmarks
 
-  validates :title, presence: true,runiqueness: true
+  validates :title, presence: true,uniqueness: true
   validates :overview, presence: true, allow_blank: false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create, :index]
   end
 end
